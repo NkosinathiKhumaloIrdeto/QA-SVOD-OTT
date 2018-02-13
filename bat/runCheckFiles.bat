fb8c6166c3e9583bfbc5cce2db04e3645ca85c54
CORP\nkosinathi.khumalo <Nkosinathi.Khumalo@irdeto.com>
2018-02-13 13:18:19 +0200

    Changed file path for runchecks

--- bat/runCheckFiles.bat
+++ bat/runCheckFiles.bat
@@ -1,1 +1,1 @@
-newman run ../automation/collections/Collection_MVP_SVODOTT.postman_collection_1.json && "../collection/run send approval.bat"
+newman run ../automation/collections/Collection_MVP_SVODOTT.postman_collection_1.json
